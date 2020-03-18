9c76f87c2d01a8683b06604388043c845225c1a4
erkang123 <sz_yonkan@163.com>
2020-03-18 13:56:17 +0800

    分支上传

--- my_blog/my_blog/settings.py
+++ my_blog/my_blog/settings.py
@@ -165,9 +165,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'collected_static')
 # SMTP服务器，改为你的邮箱的smtp!
 EMAIL_HOST = 'smtp.qq.com'
 # 改为你自己的邮箱名！
-EMAIL_HOST_USER = 'xxxxxxxx@qq.com'
+EMAIL_HOST_USER = 'xx@qq.com'
 # 你的邮箱密码
-EMAIL_HOST_PASSWORD = 'xxxxxxxxx'
+EMAIL_HOST_PASSWORD = 'xx'
 # 发送邮件的端口
 EMAIL_PORT = 25
 # 是否使用 TLS
